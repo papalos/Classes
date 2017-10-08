4a9a493eb378c4e83ade982c6f694fbfa03f5e46
Athanasev Sergius <patro1@yandex.ru>
2017-10-08 19:59:52 +0300

    + Wagon & Car

--- Building/Transport.cs
+++ Building/Transport.cs
@@ -10,21 +10,22 @@ namespace Building
 
     abstract class Transport
     {
-        protected Driver driver;
-        protected int gas;           //Запас топлива в литрах или в кг (для повозки)
-        protected int maxDistance;   //Запас хода в км
-        protected char document;     //Категория прав 
-        protected int health;        //Износ  в единицах здоровья
+        protected Driver driver;      //Опыт водителя
+        protected double gas;         //Запас топлива в литрах или в кг (для повозки)
+        protected int maxDistance;
+        protected int distance;       //Запас хода в км
+        protected char document;      //Категория прав 
+        protected int health;         //Износ  в единицах здоровья
 
-        public Transport(int gas, int maxDistance, char document, int health)
+        public Transport(double gas, int maxDistance, char document, int health)
         {
             this.gas = gas;
-            this.maxDistance = maxDistance;
+            this.distance = this.maxDistance = maxDistance;
             this.document = document;
             this.health = health;
         }
 
-        public int getGas()
+        public double getGas()
         {
             return gas;
         }
@@ -43,7 +44,7 @@ namespace Building
         {
             return health;
         }
-
+        
         /// <summary>
         /// Описывает движение транспорта
         /// </summary>
@@ -54,7 +55,7 @@ namespace Building
         /// Заправка или кормежка
         /// </summary>
         /// <param name="volume">объем топлива в литрах или в кг (для повозки)</param>
-        abstract public void refuel(int volume);
+        abstract public void refuel(double volume);
 
         /// <summary>
         /// Отдых для востановления запаса хода
@@ -65,14 +66,179 @@ namespace Building
 
     class Wagon : Transport
     {
-        public Wagon(Driver driver) : base(4,10,'N',5) { this.driver = driver; }
+        bool flag = true;    //прервать движение если один из параметров достиг минимума
 
+        /// <summary>
+        /// Создаем Лошадь 
+        /// </summary>
+        /// <param name="driver"></param>
+        public Wagon(Driver driver) : base(4d,10,'N',100) { this.driver = driver; }
+
+        /// <summary>
+        /// Сообщение о снижение характеристики ниже критического значения
+        /// </summary>
+        private string writeMessage()
+        {
+            string msg="";
+            if (gas < 0.25)
+            {
+                msg += " Лошадь не может двигаться, она голодна. Покорми ее!";
+                flag = false;
+            }
+            else
+            {
+                if (maxDistance < 1)
+                {
+                    msg += " Лошадь не может больше идти, она устала. Ей нужен отдых!";
+                    flag = false;
+                }
+                else
+                {
+                    if (health <= 0)
+                    {
+                        msg += " Лошадь - старая, она скоро сдохнет!";
+                        flag = false;
+                    }
+                }
+            }
+            return msg;
+        }
+
+        /// <summary>
+        /// Двигает лошадь и расходует ресурсы
+        /// </summary>
+        /// <param name="range">Дистанция перемещения в киллометрах</param>
         public override void move(int range)
         {
-            //Дописать реализацию чтобы при движении иссекал запас хода усталость запускаем метод отдых
-            //движение расходовало еду запускался метод кормление
-            //водитель оказывает прямое действие на износ
+            for ( ; range>0; range--)
+            {
+                Console.WriteLine(writeMessage());
+                if (flag == false) { return; }
+                distance--;
+                gas -= 0.25;
+                if(driver == Driver.FOOL)
+                {
+                    health -= 5;
+                }
+                else
+                {
+                    health -= 2;
+                }
+            }
+        }
+
+        public override void refuel(double volume)
+        {
+            gas += volume;
+        }
+        
+        public override void rest(int time)
+        {
+            distance += time / 5;
+            if (distance > maxDistance)
+            {
+                distance = maxDistance; 
+            }
         }
 
     }
+
+    class Car : Transport
+    {
+        bool flag = true;    //прервать движение если один из параметров достиг минимума
+
+        /// <summary>
+        /// Создаем Лошадь 
+        /// </summary>
+        /// <param name="driver"></param>
+        public Car(Driver driver) : base(100d, 200,'B',1000) { this.driver = driver; }
+
+        /// <summary>
+        /// Сообщение о снижение характеристики ниже критического значения
+        /// </summary>
+        private string writeMessage()
+        {
+            string msg = "";
+            if (gas < 0.25)
+            {
+                msg += " Автомобиль не может двигаться, кончился бензин. Заправь его!";
+                flag = false;
+            }
+            else
+            {
+                if (maxDistance < 1)
+                {
+                    msg += " Автомобиль не может больше ехать, двигатель перегрелся. Агрегату необходим отдых!";
+                    flag = false;
+                }
+                else
+                {
+                    if (health <= 0)
+                    {
+                        msg += " Из-за старости агрегатов или неумелого обращения автомобиль пришел в негодность!";
+                        flag = false;
+                    }
+                }
+            }
+            return msg;
+        }
+
+        /// <summary>
+        /// Двигает автомобиль и расходует ресурсы
+        /// </summary>
+        /// <param name="range">Дистанция перемещения в киллометрах</param>
+        public override void move(int range)
+        {
+            for (; range > 0; range--)
+            {
+                Console.WriteLine(writeMessage());
+                if (flag == false) { return; }
+                distance--;
+                gas -= 0.5;
+                if (driver == Driver.FOOL)
+                {
+                    health = 0;
+                }
+                else
+                {
+                    if (driver == Driver.NORMAL)
+                    {
+                        health -= 25;
+                    }
+                    else
+                    {
+                        health -= 5;
+                    }
+                }
+            }
+        }
+
+        public override void refuel(double volume)
+        {
+            gas += volume;
+        }
+
+        public override void rest(int time)
+        {
+            if (time > 20)
+            {
+                distance = maxDistance;
+                Console.WriteLine("Все нормально, можем ехать!");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточное время для охлаждения!");
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 }
